3f168ce131e770bc5d1f937641969369638d97f2
mubasshir12 <mubasshirsunni@gmail.com>
2025-09-04 18:41:50 +0000

    globe updatef

--- config.ts
+++ config.ts
@@ -3,4 +3,4 @@
  * To completely remove the developer console from the app, change this to `false`.
  * This will prevent the console components and all related logic from being included in the app.
  */
-export const IS_DEV_CONSOLE_ENABLED = true;
+export const IS_DEV_CONSOLE_ENABLED = false;
